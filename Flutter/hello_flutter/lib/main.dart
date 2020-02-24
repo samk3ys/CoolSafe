755a9430b7acc46d6fb2d5f4d24d86a6d834def4
samk3ys <47989671+samk3ys@users.noreply.github.com>
2020-02-24 01:52:13 -0500

    Play tones w/ buzzer
    Startup sound, good sound, bad sound, Mario, Doom, & Tetris

--- Flutter/hello_flutter/lib/main.dart
+++ Flutter/hello_flutter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
 void main() => runApp(MyApp());
 
---
+++ Particle/cool_safe_code/target/1.4.4/xenon/obj/src/user_module.o.d
@@ -0,0 +1,8 @@
+a:/Documents/Programming/Particle/cool_safe_code/target/1.4.4/xenon//obj/./src/user_module.o: \
+ src/user_module.c \
+ ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
+ ../../../dynalib/inc/dynalib.h
+
+../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
+
+../../../dynalib/inc/dynalib.h:
